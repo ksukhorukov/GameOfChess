a6a038df0d1b0976024a7c3f9ce12742bb7fccc0
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-05 14:02:45 +0800

    init matrix with one step

--- main.c
+++ main.c
@@ -9,13 +9,39 @@ void system_error(char *msg) {
   exit(-1);
 }
 
-void main(int argc, char** argv) {
+void print_matrix(int array[N][N]) {
   for(int i = 0; i < N; i++) {
     for(int j = 0; j < N; j++) {
-
-      matrix[i][j] = 0;
       printf("%d ", matrix[i][j]);
     }
     printf("\n");
   }
+}
+
+void main(int argc, char** argv) {
+  /* inint matrix */
+  
+  for(int i = 0; i < N; i++) {
+    for(int j = 0; j < N; j++) {
+      if(i == 0) {
+        matrix[0][j] = 1;  
+      } else {
+        matrix[i][j] = 0;
+      }
+    }    
+  }
+
+/*
+  1 1 1 1 1 1 1 1 
+  0 0 0 0 0 0 0 0 
+  0 0 0 0 0 0 0 0 
+  0 0 0 0 0 0 0 0 
+  0 0 0 0 0 0 0 0 
+  0 0 0 0 0 0 0 0 
+  0 0 0 0 0 0 0 0 
+  0 0 0 0 0 0 0 0 
+*/
+
+  print_matrix(matrix);
+
 }
