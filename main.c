6f7ae0110bf88603b58f4d27939d193894cd9e12
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-05 14:39:32 +0800

    need to debug

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ int check_horizontal(int i, int array[N][N]) {
   for(int k = 0; k < N; k++) {
     printf("%d ", array[i][k]);
     if(array[i][k] == 1 && k != i) {
-      continue;
+      return 0;
     }
   }
   printf("\n");
@@ -42,14 +42,25 @@ void check_horizontal_info(int i, int array[N][N]) {
   }
 }
 
-int check_vertical(int i, int array[N][N]) {
-  // for(int k = 0; k < N; k++) {
-  //   if(array[i][k] == 1) {
-  //     return 0;
-  //   }
-  // }
+int check_vertical(int j, int array[N][N]) {
+  printf("checking %d vertical\n", j);
 
+  for(int i = 0; i < N; i++) {
+    printf("%d ", array[i][j]);
+    if(array[i][j] == 1 && j != i) {
+      return 0;
+    }
+  }
+  printf("\n");
   return 1;
+}
+
+void check_vertical_info(int i, int array[N][N]) {
+  if(check_vertical(i, matrix) == TRUE) {
+    printf("positive vertical\n");
+  } else {
+    printf("negative vertical\n");
+  }
 }
 
 int check_diagonals(int i, int j, int array[N][N]) {
@@ -112,5 +123,39 @@ void main(int argc, char** argv) {
   print_matrix(matrix);
 
   check_horizontal_info(1, matrix);
+
+/*
+        1 1 1 1 1 1 1 1 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+*/
+
+  print_matrix(matrix);
+
+  check_vertical_info(0, matrix);
+  print_matrix(matrix);
+
+/*
+        0 1 1 1 1 1 1 1 
+        1 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+*/
+
+  matrix[0][0] = 0;
+  matrix[1][0] = 1;
+
+  print_matrix(matrix);
+
+  check_vertical_info(1, matrix);
 
 }
