54d2b1f7de3da8ae091b86ed658a4401f0828366
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-05 14:16:13 +0800

    stubs

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 const int N = 8;
+const int TRUE = 1;
+const int FALSE = 0;
+
 int matrix[N][N];
 
 void system_error(char *msg) {
@@ -18,6 +21,41 @@ void print_matrix(int array[N][N]) {
   }
 }
 
+int check_horizontal(int i, int array[N][N]) {
+  for(int k = 0; k < N; k++) {
+    if(array[i][k] == 1) {
+      return 0;
+    }
+  }
+
+  return 1;
+}
+
+int check_vertical(int i, int array[N][N]) {
+  // for(int k = 0; k < N; k++) {
+  //   if(array[i][k] == 1) {
+  //     return 0;
+  //   }
+  // }
+
+  return 1;
+}
+
+int check_diagonals(int i, int j, int array[N][N]) {
+  // for(int k = 0; k < N; k++) {
+  //   if(array[i][k] == 1) {
+  //     return 0;
+  //   }
+  // }
+
+  return 1;
+}
+
+
+int check_matrix(int i, int j, int array[N][N]) {
+  return check_diagonals(i, j, array) & check_horizontal(i, array) & check_vertical(j, array);
+}
+
 void main(int argc, char** argv) {
   /* inint matrix */
   
@@ -32,16 +70,21 @@ void main(int argc, char** argv) {
   }
 
 /*
-  1 1 1 1 1 1 1 1 
-  0 0 0 0 0 0 0 0 
-  0 0 0 0 0 0 0 0 
-  0 0 0 0 0 0 0 0 
-  0 0 0 0 0 0 0 0 
-  0 0 0 0 0 0 0 0 
-  0 0 0 0 0 0 0 0 
-  0 0 0 0 0 0 0 0 
+        1 1 1 1 1 1 1 1 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
+        0 0 0 0 0 0 0 0 
 */
 
   print_matrix(matrix);
 
+  if(check_horizontal(0, matrix) == TRUE) {
+    printf("positive horiznotal\n");
+  } else {
+    printf("negative horiznotal\n");
+  }
 }
